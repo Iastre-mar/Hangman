fd11e2a553bdafc9755d16174bf1ae316ce65b06
vchichvarin <vladimir150799@yandex.ru>
2025-03-27 20:14:05 +0300

    ConsoleHelper can read from console and write into it

--- src/main/java/ru/cva/hangman/ConsoleHelper.java
+++ src/main/java/ru/cva/hangman/ConsoleHelper.java
@@ -1,9 +1,25 @@
 package ru.cva.hangman;
 
+import java.util.Scanner;
+
 /**
  * Класс для работы с консолью, т.к. сама игра может быть реализована
  *  как в консоли, так и через другие интерфейсы
  */
 public class ConsoleHelper {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String readString() {
+        return scanner.nextLine();
+    }
+
+    public int readInt() {
+        return scanner.nextInt();
+    }
+
+    public void writeMessage(String message) {
+        System.out.println(message);
+    }
+
 
 }
