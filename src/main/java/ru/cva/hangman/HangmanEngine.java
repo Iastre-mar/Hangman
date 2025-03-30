18b2108aac8ae6ff3688a654abaf67d247f061af
vchichvarin <vladimir150799@yandex.ru>
2025-03-30 12:32:32 +0300

    HangmanEngine added check for entering a letter multiple times

--- src/main/java/ru/cva/hangman/HangmanEngine.java
+++ src/main/java/ru/cva/hangman/HangmanEngine.java
@@ -2,7 +2,9 @@ package ru.cva.hangman;
 
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class HangmanEngine implements Engine {
@@ -11,10 +13,9 @@ public class HangmanEngine implements Engine {
     private int lifeCountLeft;
 
     // Все буквы хранятся внутри в нижнем регистре
-    private  Map<Character, Boolean> charactersGuessed;
+    private Map<Character, Boolean> charactersGuessed;
 
-    //private Set<Character> typed
-
+    private Set<Character> enteredChars;
 
 
     /**
@@ -28,16 +29,19 @@ public class HangmanEngine implements Engine {
         currentWord = WordsHub.getWord();
 
         charactersGuessed = new HashMap<>();
-        currentWord.getHiddenWord().chars()
-                  .mapToObj(i -> Character.toLowerCase((char) i))
-                  .forEach(c -> charactersGuessed.put(c, false));
+        currentWord.getHiddenWord()
+                   .chars()
+                   .mapToObj(i -> Character.toLowerCase((char) i))
+                   .forEach(c -> charactersGuessed.put(c, false));
+
+        enteredChars = new HashSet<>();
 
 
     }
 
     @Override
     public void playRound() {
-        ConsoleHelper.writeMessage("Я загадал слово ", getMask());
+        ConsoleHelper.writeMessage("Я загадал слово", getMask());
         ConsoleHelper.writeMessage("Попробуй угадать букву:");
 
 
@@ -50,23 +54,21 @@ public class HangmanEngine implements Engine {
                 ConsoleHelper.writeMessage(
                         "Поздравляю! Ты угадал букву верно");
             } else {
-                lifeCountLeft--;
-                ConsoleHelper.writeMessage(
-                        "Сожалею, ты не угадал букву,",
-                        " количество оставшихся попыток: ",
-                        String.valueOf(lifeCountLeft));
+                lifeCountLeft -= checkCharAlreadyEntered(userInput);
+                ConsoleHelper.writeMessage("Сожалею, ты не угадал букву,",
+                                           "количество оставшихся попыток:",
+                                           String.valueOf(lifeCountLeft));
             }
 
-            if (lifeCountLeft <= 0){
+            if (lifeCountLeft <= 0) {
                 break;
             }
 
             ConsoleHelper.writeMessage("Слово:", getMask());
         }
 
-        ConsoleHelper.writeMessage(
-                "Полное слово: ", currentWord.getHiddenWord());
-
+        ConsoleHelper.writeMessage("Полное слово:",
+                                   currentWord.getHiddenWord());
 
         ConsoleHelper.writeMessage("Очередной увлекательный кон завершен");
     }
@@ -77,17 +79,19 @@ public class HangmanEngine implements Engine {
     }
 
 
-    public String getMask() {
-        return currentWord.getHiddenWord().chars()
-                         .mapToObj(i -> Character.toLowerCase((char) i))
-                         .map(c -> charactersGuessed.get(
-                                 c) ? c.toString() : "*")
-                         .collect(Collectors.joining());
+    private String getMask() {
+        return currentWord.getHiddenWord()
+                          .chars()
+                          .mapToObj(i -> Character.toLowerCase((char) i))
+                          .map(c -> charactersGuessed.get(
+                                  c) ? c.toString() : "*")
+                          .collect(Collectors.joining());
     }
 
-    public boolean checkLetter(char letter) {
+    private boolean checkLetter(char letter) {
         // Внутри программы все буквы приведены к нижнему регистру
         letter = Character.toLowerCase(letter);
+
         boolean res = charactersGuessed.containsKey(letter);
 
         if (res) {
@@ -97,15 +101,34 @@ public class HangmanEngine implements Engine {
     }
 
 
-    public boolean checkExistenceNotGuessedLetters() {
+    private boolean checkExistenceNotGuessedLetters() {
         return !charactersGuessed.values()
                                  .stream()
-                                 .filter(b -> b == false)
+                                 .filter(b -> !b)
                                  .toList()
                                  .isEmpty();
     }
 
     private void openLetter(char letter) {
         charactersGuessed.put(letter, true);
+    }
+
+    /**
+     * @param letter
+     * @return 1 в случае если буква вводится в первый раз
+     * или 0 если уже была введена ранее
+     */
+
+    private int checkCharAlreadyEntered(char letter) {
+        letter = Character.toLowerCase(letter);
+        int indicatorLetterAlreadyEntered = 0;
+        if (enteredChars.add(letter)) {
+            indicatorLetterAlreadyEntered = 1;
+        } else {
+            ConsoleHelper.writeMessage("Буква", String.valueOf(letter),
+                                       "уже вводилась ранее");
+        }
+        return indicatorLetterAlreadyEntered;
+
     }
 }
